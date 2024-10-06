39e4fcc6fafb65fe48c369b64502cd9070c82c9f
TTQBqueen <ttboler@uncg.edu>
2024-10-06 13:57:10 -0400

    animal class is made

---
+++ 340-HomeWork/demo/src/test/java/com/HW4/demo/Csc340Hw4ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.HW4.demo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Csc340Hw4ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
