d5137954fc4ef8715d870d8807fc29577f015280
TTQBqueen <ttboler@uncg.edu>
2024-11-05 23:31:42 -0500

    Class backend

--- UNCG-Fitness-Connect/src/main/java/com/UNCG_Fitness/UNCG_Fitness_Connect/fitnessClass/ClassController.java
+++ UNCG-Fitness-Connect/src/main/java/com/UNCG_Fitness/UNCG_Fitness_Connect/fitnessClass/ClassController.java
@@ -6,42 +6,140 @@ import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.*;
 
-@Controller
+import java.util.List;
+
+@RestController
 @RequestMapping("/classes")
 public class ClassController {
 
     @Autowired
     ClassService classService;
-    
+
     @Autowired
     UserService userService;
+
+    @Autowired
+//    SubscriptionService subsService;
     
 //    All Class
-@GetMapping("/all")
-public String getAllClasses(Model model) {
-    model.addAttribute("classList", classService.getAllClasses());
-    model.addAttribute("title", "All Class");
-    return "/Class/class-list";
-}
-
-//    Look up User class based on ID
-@GetMapping("/user/{userId}")
-public String getClassesByUser(@PathVariable long userId,Model model) {
-    model.addAttribute("classList", classService.getAllUserClasses(userId));
-    model.addAttribute("title", "Your Classes: "+userId);
-    return "/Class/inst_view";
-}
-//Class by ID
-    @GetMapping("/{classId}")
-    public String getClassesById(@PathVariable int classId, Model model) {
-        model.addAttribute("class", classService.getClassById(classId));
-        model.addAttribute("title", "Class # "+classId+" Details");
-        return "/Class/class-details";
+    /**
+     * Get a list of all Classes .
+     * http://localhost:8080/classes/all
+     *
+     * @return a list of Class  objects.
+     */
+    @GetMapping("/all")
+    public List<Class> getAllClasses() {
+        return classService.getAllClasses();
     }
 
-//    Create Class
-    
-//    Update
-    
-//    Delete
+
+    /**
+     * Get a specific class by Id.
+     * http://localhost:8080/classes/2
+     *
+     * @param classId the unique Id for a Classes.
+     * @return One Class object.
+     */
+    @GetMapping("/{classId}")
+    public Class getOneClass(@PathVariable int classId) {
+        return classService.getClassById(classId);
+    }
+
+
+//    IDK if it work until i get the user table
+    /**
+     * Get a specific class by Id.
+     * http://localhost:8080/classes/2
+     *
+     * @param creatorId the unique Id for a Classes.
+     * @return A list of Classes objects matching the creatorId key..
+     */
+    @GetMapping("INSTRUCTOR/{creatorId}")
+    public List<Class> getClassesByCreatorId(@PathVariable int creatorId) {
+        return classService.getClassesByCreatorId(creatorId);
+    }
+
+
+
+
+
+    /**
+     * Get a list of Classes based on their level.
+     * http://localhost:8080/classes/level
+     *
+     * @param level the search key.
+     * @return A list of Classes objects matching the level key.
+     */
+
+    @GetMapping("/levels")
+    public List<Class> getClassesByLevel(@RequestParam(name = "level", defaultValue = "BEGINNER") String level) {
+        return classService.getClassesByLevel(level);
+    }
+
+    /**
+     * Get a list of Classes based on their name.
+     * http://localhost:8080/classes?title=yoga
+     *
+     * @param title the search key.
+     * @return A list of Class objects matching the search key.
+     */
+    @GetMapping("")
+    public List<Class> getClassesByName(@RequestParam(name = "title", defaultValue = "yoga") String title) {
+        return classService.getClassesByTitle(title);
+    }
+
+    /**
+     * Get a list of Classes based on their title.
+     * http://localhost:8080/classes/search?title=yoga
+     *
+     * @param title the search key.
+     * @return A list of Class objects matching the search key.
+     */
+    @GetMapping("/search")
+    public List<Class> getClassesByNameContains(@RequestParam(name = "title", required = false) String title) {
+        return classService.getClassesByTitleContains(title);
+    }
+
+    /**
+     * Create a new Classes entry.
+     * http://localhost:8080/classes/new --data '{ "ClassId": 4, "name": "sample4", "major": "csc", "gpa": 3.55}'
+     *
+     * @param fitnessClass the new Class object.
+     * @return the updated list of Classes.
+     */
+    @PostMapping("/new")
+    public List<Class> addNewClass(@RequestBody Class fitnessClass) {
+        classService.addNewClass(fitnessClass);
+        return classService.getAllClasses();
+    }
+
+    /**
+     * Update an existing Class object.
+     * http://localhost:8080/Classes/update/2 --data '{
+     * "classId": 1, "title": "cat", "ScientificName": "Felion", "Classification": "manmmal", "Habitat": "outside" , "Description": "this is a cat" }'
+     *
+     * @param classId the unique Class Id.
+     * @param fitnessClass the new update Class details.
+     * @return the updated Class object.
+     */
+    @PutMapping("/update/{classId}")
+    public Class updateClass(@PathVariable int classId, @RequestBody Class fitnessClass) {
+        classService.updateClass(classId,fitnessClass);
+        return classService.getClassById(classId);
+    }
+
+    /**
+     * Delete a Animal object.
+     * http://localhost:8080/classes/delete/2
+     *
+     * @param classId the unique Animal Id.
+     * @return the updated list of Animals.
+     */
+    @DeleteMapping("/delete/{classId}")
+    public List<Class> deleteClassById(@PathVariable int classId) {
+        classService.deleteClassById(classId);
+        return classService.getAllClasses();
+    }
+
 }
--- UNCG-Fitness-Connect/src/main/java/com/UNCG_Fitness/UNCG_Fitness_Connect/fitnessClass/ClassService.java
+++ UNCG-Fitness-Connect/src/main/java/com/UNCG_Fitness/UNCG_Fitness_Connect/fitnessClass/ClassService.java
@@ -3,23 +3,68 @@ package com.UNCG_Fitness.UNCG_Fitness_Connect.fitnessClass;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
+import java.util.List;
+
 @Service
 public class ClassService {
 
     @Autowired
     ClassRepository classRepository;
 
-    public Object getAllClasses() {
+    // Fetch all Classes.
+    public List<Class> getAllClasses() {
         return classRepository.findAll();
     }
 
-    public Object getAllUserClasses(long userId) {
-        // Using a static userId since we are the only user
-        return classRepository.getUserClasses(userId);
+    // Fetch a single Classes by ID.
+    public Class getClassById(int classId) {
+        return classRepository.findById(classId).orElse(null);
     }
 
-    public Class getClassById(int id) {
-        return classRepository.findById(id).orElse(null);
+    //fetch class by title
+    public List<Class> getClassesByTitle(String title) {
+        return classRepository.getClassesByTitle(title);
+    }
+    //fetch classes by creatorID
+    public List<Class> getClassesByCreatorId(int creatorId) {
+        return classRepository.getClassesByCreatorId(creatorId);
+    }
+
+    // Fetch Classes by level.
+    public List<Class> getClassesByLevel(String level) {
+        return classRepository.getClassesByLevel(level);
+    }
+
+    // Fetch an class by part of a title
+    public List<Class> getClassesByTitleContains(String title) {
+        return classRepository.findByTitleContainingIgnoreCase(title);
+    }
+    // add Class .
+    public void addNewClass(Class fitnessClass) {classRepository.save(fitnessClass);
+    }
+
+    // update existing Class.
+    public Class updateClass(int classID, Class fitnessClass) {
+        Class existingClass = getClassById(classID);
+        existingClass.setTitle(fitnessClass.getTitle());
+        existingClass.setCreator(fitnessClass.getCreator());
+        existingClass.setDetails(fitnessClass.getDetails());
+        existingClass.setDate(fitnessClass.getDate());
+        existingClass.setStartTime(fitnessClass.getStartTime());
+        existingClass.setEndTime(fitnessClass.getEndTime());
+        existingClass.setLevel(fitnessClass.getLevel());
+        existingClass.setInstructorName(fitnessClass.getInstructorName());
+        existingClass.setStatus(fitnessClass.getStatus());
+        existingClass.setImagePath(fitnessClass.getImagePath());
+        existingClass.setUpdatedAt(fitnessClass.getUpdatedAt());
+
+        return classRepository.save(existingClass);
+    }
+    // delete class
+    public String deleteClassById(int classId) {
+        classRepository.deleteById(classId);
+        return "Class removed !! " + classId;
     }
 
 }
+
