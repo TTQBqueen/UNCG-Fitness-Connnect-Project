ef8d021e2e07bc36bc768d798e22928b77629df7
TTQBqueen <ttboler@uncg.edu>
2024-11-05 23:52:51 -0500

    fixed

--- UNCG-Fitness-Connect/src/main/java/com/UNCG_Fitness/UNCG_Fitness_Connect/fitnessClass/ClassService.java
+++ UNCG-Fitness-Connect/src/main/java/com/UNCG_Fitness/UNCG_Fitness_Connect/fitnessClass/ClassService.java
@@ -66,13 +66,6 @@ public class ClassService {
         return "Class removed !! " + classId;
     }
 
-    public void createNewGoal(Class class) {
-        if (classRepository.existsById(class.getClassId())) {
-
-        }
-        class = new Class(class.getUser(), class.getTitle(), class.getDetails(), class.getTargetDate());
-        classRepository.save(class);
-    }
 
 
 }
