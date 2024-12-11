46d60f62d98a285e33260b0be8576f5007092c9a
jenniferxgt <157390766+jenniferxgt@users.noreply.github.com>
2024-12-11 15:25:43 -0500

    committing report changes

--- UNCG-Fitness-Connect/src/main/java/com/UNCG_Fitness/UNCG_Fitness_Connect/report/ReportService.java
+++ UNCG-Fitness-Connect/src/main/java/com/UNCG_Fitness/UNCG_Fitness_Connect/report/ReportService.java
@@ -13,37 +13,37 @@ import java.util.Optional;
 public class ReportService {
 
     @Autowired
-    private com.UNCG_Fitness.UNCG_Fitness_Connect.report.reportRepository reportRepository;
+    private ReportRepository reportRepository;
 
     public List<Report> getAllReports() {
         return reportRepository.findAll();
     }
 
     public Report getReportById(int reportId) {
-        return com.UNCG_Fitness.UNCG_Fitness_Connect.report.reportRepository.findById(reportId).orElse(null);
+        return reportRepository.findById(reportId).orElse(null);
     }
 
     public List<Report> getReportByType(String reportType) {
-        return com.UNCG_Fitness.UNCG_Fitness_Connect.report.reportRepository.findByReportType(reportType);
+        return reportRepository.findByReportType(reportType);
     }
 
     public Report addNewReport(Report report) {
         report.setCreatedAt(new Date());
-        return com.UNCG_Fitness.UNCG_Fitness_Connect.report.reportRepository.save(report);
+        return reportRepository.save(report);
     }
 
     public Report updateReportStatus(int reportId, String status) {
-        Optional<Report> reportOptional = com.UNCG_Fitness.UNCG_Fitness_Connect.report.reportRepository.findById(reportId);
+        Optional<Report> reportOptional = reportRepository.findById(reportId);
         if (reportOptional.isPresent()) {
             Report report = reportOptional.get();
             report.setStatus(status);
             report.setUpdatedAt(new Date());
-            return com.UNCG_Fitness.UNCG_Fitness_Connect.report.reportRepository.save(report);
+            return reportRepository.save(report);
         }
         return null;
     }
 
     public void deleteReportById(int reportId) {
-        com.UNCG_Fitness.UNCG_Fitness_Connect.report.reportRepository.deleteById(reportId);
+        reportRepository.deleteById(reportId);
     }
 }
